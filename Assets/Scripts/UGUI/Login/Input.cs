64280dc22b4857b347f352c435ffa7fa0189f9b2
zhangyawei <yawei_zhang@foxmail.com>
2016-08-11 21:07:45 +0800

    login select inputfield

--- Assets/Scripts/UGUI/Login/Input.cs
+++ Assets/Scripts/UGUI/Login/Input.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
-public class Input : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+using UnityEngine.UI;
+public class Input : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
-
+    GameObject _placeholder;
+    GameObject _inputText;
 	// Use this for initialization
 	void Start ()
     {
-
+        var tss = GetComponentsInChildren<Transform>();
+        foreach (var ts in tss)
+        {
+            if (ts.gameObject.name == "Placeholder")
+            {
+                _placeholder = ts.gameObject;
+            }
+            else if (ts.gameObject.name == "Text")
+            {
+                _inputText = ts.gameObject;
+            }
+        }
     }
 	
 	// Update is called once per frame
@@ -15,16 +28,20 @@ public class Input : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
 	
 	}
-
-    public void OnPointerDown(PointerEventData eventData)
+    public void OnSelect(BaseEventData eventData)
     {
-
+        if (_placeholder)
+        {
+            _placeholder.SetActive(false);
+        }
     }
-    public void OnPointerUp(PointerEventData eventData)
+    public void OnDeselect(BaseEventData eventData)
     {
-
+        if (_placeholder)
+        {
+            _placeholder.SetActive(true);
+        }
     }
-
 
 
 }
