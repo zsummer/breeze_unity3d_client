7a575f3585b361f477fc1ca576eacf9a4c13ae0c
zsummer <zhangyawei@zhangyawei.local>
2016-08-28 19:32:29 +0800

    mouse hit

--- Assets/Scripts/Avatar/AvatarController.cs
+++ Assets/Scripts/Avatar/AvatarController.cs
@@ -1,12 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+
+
+enum MoveType
+{
+	MT_IDLE,
+	MT_HANDLE,
+	MT_TARGET,
+}
 public class AvatarController : MonoBehaviour
 {
 
     private AnimationState idle;
     private AnimationState runned;
     private Animation anim;
+
+
+	RaycastHit _hit3D = new RaycastHit();
+	Camera _mainCamera;
+
+	private Vector3 _targetPos;
+	private MoveType _moving = MoveType.MT_IDLE;
+	public float _speed = 7.0f;
+
 	void Start ()
     {
         anim = GetComponent<Animation>();
@@ -14,7 +31,53 @@ public class AvatarController : MonoBehaviour
         runned = anim["walk"];
         idle.wrapMode = WrapMode.Loop;
         runned.wrapMode = WrapMode.Loop;
+		_mainCamera = Camera.main;
     }
+
+	void MoveControl()
+	{
+		if (_moving == MoveType.MT_IDLE || _moving == MoveType.MT_HANDLE) 
+		{
+			float h = Input.GetAxis("Horizontal");
+			float v = Input.GetAxis("Vertical");
+			if (Math.Abs(h)> 0.1 || Math.Abs(v) > 0.1) 
+			{
+				_targetPos = transform.position + new Vector3 (h, 0, v) * 10;
+				_moving = MoveType.MT_HANDLE;
+			}
+
+		}
+		if (_moving == MoveType.MT_HANDLE) 
+		{
+			float h = Input.GetAxis("Horizontal");
+			float v = Input.GetAxis("Vertical");
+			if (Math.Abs(h)< 0.1 && Math.Abs(v) < 0.1) 
+			{
+				_moving = MoveType.MT_IDLE;
+			}
+		}
+		if (_moving == MoveType.MT_IDLE || _moving == MoveType.MT_TARGET)
+		{
+			if (Input.GetButtonDown("Fire1")) 
+			{
+				Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+				Physics.Raycast(ray, out _hit3D, 100);
+				if (_hit3D.transform != null && _hit3D.transform.name == "Terrain")
+				{
+					Debug.Log(_hit3D.transform.gameObject.name);
+					_targetPos = _hit3D.point;
+					_moving = MoveType.MT_TARGET;
+				}
+			}
+		}
+		if (_moving == MoveType.MT_TARGET)
+		{
+			if (Vector3.Distance(_targetPos, transform.position) < 0.1) 
+			{
+				_moving = MoveType.MT_IDLE;
+			}
+		}
+	}
 
     void FixedUpdate()
     {
@@ -22,33 +85,37 @@ public class AvatarController : MonoBehaviour
         {
             return;
         }
+		MoveControl ();
+		if (_moving == MoveType.MT_IDLE && anim.IsPlaying(runned.name)) 
+		{
+			anim.CrossFade(idle.name, 0.2f);
+		}
+		if (_moving != MoveType.MT_IDLE) 
+		{
+			if (!anim.IsPlaying(runned.name))
+			{
+				anim.CrossFade(runned.name, 0.2f);
+			}
 
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+			var mdis = _speed * Time.fixedDeltaTime;
+			var cdis = Vector3.Distance (transform.position, _targetPos);
+			if (mdis < cdis) 
+			{
+				var rat = mdis / cdis;
+				transform.localPosition += (_targetPos - transform.position) * rat;
+				transform.LookAt(_targetPos);
+			} 
+			else 
+			{
+				transform.localPosition = _targetPos;
+			}
+		}
 
-        if (System.Math.Abs(h) < 0.1 && System.Math.Abs(v) < 0.1)
-        {
-            if (anim.IsPlaying(runned.name))
-            {
-                anim.CrossFade(idle.name, 0.2f);
-            }
-            return;
-        }
-        Vector3 velocity = new Vector3(h, 0, v);
-
-        if (!anim.IsPlaying(runned.name))
-        {
-            anim.CrossFade(runned.name, 0.2f);
-        }
-        velocity *= 7.0f;
-
-        transform.LookAt(transform.position + velocity * Time.fixedDeltaTime);
-        transform.localPosition += velocity * Time.fixedDeltaTime;
 
     }
 	// Update is called once per frame
 	void Update ()
     {
-	
+		
 	}
 }
