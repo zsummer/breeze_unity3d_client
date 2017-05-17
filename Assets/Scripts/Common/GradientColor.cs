18527c11f921614620d86507524b3d50057636bf
zsummer <yawei.zhang@foxmail.com>
2017-05-17 08:19:18 +0800

    up

---
+++ Assets/Scripts/Common/GradientColor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public class GradientColor
+{
+
+     float[] counter_step = new float[3] { 0.01f, 0.003f, 0.001f };
+     float[] counter = new float[3] { 0, 0, 0 };
+
+     float[] color_step = new float[3] { 255, 0, 255 };
+     float[] color = new float[3] { 255, 255, 255 };
+     Color cacheColor = new Color(0, 0, 0);
+    public  Color fetchColor()
+    {
+        return cacheColor;
+    }
+    public  void refresh()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            counter[i] += counter_step[i];
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            if (counter[i] >= 1)
+            {
+                counter[i] = 0;
+                color[i] += color_step[i];
+
+                if (color[i] > 255)
+                {
+                    color[i] = 255;
+                    if (color_step[i] >0 )
+                    {
+                        color_step[i] *= -1;
+                    }
+                }
+                if (color[i] < 0)
+                {
+                    color[i] = 0;
+                    if (color_step[i] < 0)
+                    {
+                        color_step[i] *= -1;
+                    }
+                }
+            }
+        }
+
+        cacheColor.r = color[0];
+        cacheColor.g = color[1];
+        cacheColor.b = color[2];
+    }
+}
+
---
+++ Assets/Scripts/Common/MainScreenLabel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class MainScreenLabel
+{
+    static GradientColor gradient = new GradientColor();
+    static GUIStyle guiStyle = new GUIStyle();
+    static Vector2 labelSize = new Vector2();
+    public static void Preprocess()
+    {
+        gradient.refresh();
+        guiStyle.normal.textColor = gradient.fetchColor();
+        guiStyle.richText = true;
+        guiStyle.normal.background = null;
+        guiStyle.fontSize = (int)(Screen.height * GameOption._fontSizeScreeHeightRate);
+        labelSize.x = guiStyle.fontSize;
+        labelSize.y = guiStyle.fontSize;
+    }
+
+    public static void Label(ref string label)
+    {
+        Vector2 nameSize = GUI.skin.label.CalcSize(new GUIContent(label)) * guiStyle.fontSize / GUI.skin.font.fontSize;
+        labelSize.y += nameSize.y;
+        GUI.Label(new Rect(labelSize.x, labelSize.y, nameSize.x, nameSize.y), label, guiStyle);
+    }
+}
+
+
